ea0a17f4fc3fa97d28d4c159d59f8f01c9950bf4
Juan David Acosta Muñoz <juandavid.a27@gmail.com>
2026-05-11 17:56:52 +0200

    refactor: organize commands and add error handling

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,24 +1,11 @@
-import { setUser } from "./config.js";
-
 export type CommandHandler = (cmdName: string, ...args: string[]) => void;
-
-export function handlerLogin(cmdName: string, ...args: string[]) {
-  if (args.length === 0) {
-    console.log("username is required");
-    process.exit(1);
-  }
-  const userName = args[0];
-  setUser(userName);
-  console.log(`User ${userName} has been set`);
-}
-
 export type CommandsRegistry = Record<string, CommandHandler>;
 
 export function registerCommand(
   registry: CommandsRegistry,
   cmdName: string,
   handler: CommandHandler,
-) {
+): void {
   registry[cmdName] = handler;
 }
 
@@ -26,7 +13,10 @@ export function runCommand(
   registry: CommandsRegistry,
   cmdName: string,
   ...args: string[]
-) {
+): void {
   const command = registry[cmdName];
+  if (!command) {
+    throw new Error(`Unknown command: ${cmdName}`);
+  }
   command(cmdName, ...args);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
-import type { CommandsRegistry } from "./commands.js";
-import { registerCommand, handlerLogin, runCommand } from "./commands.js";
+import type { CommandsRegistry } from "./commands/commands.js";
+import { registerCommand, runCommand } from "./commands/commands.js";
+import { handlerLogin } from "./commands/userCommands.js";
 
 function main() {
-  const registry: CommandsRegistry = {};
-  registerCommand(registry, "login", handlerLogin);
-
   const args = process.argv.slice(2);
   if (args.length === 0) {
     console.log("Not enough arguments were provided");
+    console.log("usage: cli <command> [args...]");
     process.exit(1);
   }
 
   const cmdName = args[0];
   const argsArr = args.slice(1);
-  runCommand(registry, cmdName, ...argsArr);
+  const registry: CommandsRegistry = {};
+
+  registerCommand(registry, "login", handlerLogin);
+  try {
+    runCommand(registry, cmdName, ...argsArr);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(`Error running command ${cmdName}: ${err.message}`);
+    } else {
+      console.error(`Error running command ${cmdName}: ${err}`);
+    }
+    process.exit(1);
+  }
 }
 
 main();
